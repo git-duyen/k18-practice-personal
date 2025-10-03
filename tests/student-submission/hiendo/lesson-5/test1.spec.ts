682d97938d0d767fea967692ae534b833460053b
PW-Hien <dothanhhien2409@gmail.com>
2025-10-03 16:43:54 +0700

    chore: apply code review suggestions (round 4)

--- tests/student-submission/hiendo/lesson-5/test1.spec.ts
+++ tests/student-submission/hiendo/lesson-5/test1.spec.ts
@@ -4,7 +4,7 @@ const testData = {
     username: 'hiendo',
     email: 'dothanhhien2409@gmail.com',
     dob: '01011999',
-    file: 'tests\student-submission\hiendo\data-test\file1.txt',
+    file: 'tests/student-submission/hiendo/data-test/file1.txt',
     bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
     customDate: '12102025',
     interests: 'music',
