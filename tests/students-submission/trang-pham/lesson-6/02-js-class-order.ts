06341f17ebe53c413e1c22aca6948a4dead10bc8
test 01 - Trang Pham <trangptt315@gmail.com>
2026-06-15 16:39:36 +0700

    feat: Adding solution for lesson 6

---
+++ tests/students-submission/trang-pham/lesson-6/02-js-class-order.ts
@@ -0,0 +1,70 @@
+interface Items {
+    name: string;
+    price: number;
+    amount: number;
+    discount: number;
+}
+
+class Order {
+    orderID: string;
+    customerName: string;
+    items: Items[];
+    totalAmount: number;
+
+    constructor(
+        orderID: string,
+        customerName: string
+    ){
+        this.orderID = orderID;
+        this.customerName = customerName;
+        this.items = [];
+        this.totalAmount = 0;
+    }
+
+    addItem(item: Items){
+        this.items.push(item);
+        return this.items;
+    }
+
+    calculateTotalForEach(orderItems: Items[]) {
+        return orderItems.map(item => {
+            const totalAmount = item.price * item.amount * (1 - item.discount / 100);
+        
+            return {
+                ...item,
+                totalAmount
+            };
+        })
+    }
+
+    calculateTotalAmount(orderItems: ItemsAmount[]){
+        return orderItems.reduce((total, item) => {
+            return total + item.totalAmount;
+        }, 0);
+    }
+}
+
+const orderItems = [
+    {name: 'laptop', price: 20_000, amount: 6, discount: 10},
+    {name: 'monitor',price: 5_000, amount: 10, discount: 5},
+    {name: 'mouse', price: 2_000, amount: 8, discount: 0}
+]
+const order = new Order('Ord_01', 'Victor');
+for(let item of orderItems){
+    order.addItem(item);
+}
+// console.log(order);
+interface ItemsAmount {
+    name: string;
+    price: number;
+    amount: number;
+    discount: number;
+    totalAmount: number;
+}
+
+let itemAmount = order.calculateTotalForEach(orderItems);
+console.log(`Total amount is`, order.calculateTotalAmount(itemAmount));
+
+
+
+
