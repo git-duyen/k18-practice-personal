2a6489ccd7d9676ed3d11324bf65db47c30266f5
= <trilm17fs@gmail.com>
2026-06-19 15:35:07 +0700

    fix: fix comnment PR and delete duplicated folder

--- tests/student-submission/Bryan/lesson-04/final-stage.js
+++
@@ -1,8 +0,0 @@
-
-for (let i = 1; i <= 100; i++) {
-  for (let j = i; j <= 100; j++) {
-    if ((i + j) % 17 === 0) {
-      console.log(`(${i}, ${j}) là cap so co tong chia het cho 17`);
-    }
-  }
-}
