bb5beb1941df4a6a0c5d31ea717d619647ba571f
Thao Nguyen <npthao.zb@gmail.com>
2026-07-30 14:08:57 +0700

    lesson-8-note

---
+++ tests/students-submission/npthao/lesson-08/01-material-site.spec.ts
@@ -0,0 +1,45 @@
+import { test , expect } from "@playwright/test" ;
+
+test.describe("Material site" , async() => {
+    test.beforeAll(async({ page }) => {
+        await page.goto("https://playwrightvn.com/");
+
+    });
+
+    test.beforeEach(async({ page }) => {
+        await test.step("Go to material page" , async() => {
+            await page.goto("https://material.playwrightvn.com/");
+        });
+    });
+
+    test.afterEach(async({page}) => {
+        await page.goto("https://google.com/");
+    });
+
+    test.afterAll(async({ page }) => {
+        console.log("All Done");
+    });
+
+    test("User registration page", async( {page}) => {
+        // await test.step("Go to material page" , async() => {
+        //     await page.goto("https://material.playwrightvn.com/");
+        // });
+    
+
+        await test.step ("Click to User registration page" , async() => {
+            await page.locator("//a[@href='01-xpath-register-page.html']").click();
+        });
+    });
+
+    test("Product page", async({ page }) => {
+        // await test.step("Go to material Page" , async() => {
+        //     await page.goto("https://material.playwrightvn.com/");
+        // });
+
+        await test.step("Click to product page" , async() => {
+            await page.locator("//a[@href='02-xpath-product-page.html']").click();
+        });
+    });
+
+});
+
---
+++ tests/students-submission/npthao/lesson-08/02-assertion.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test';
+
+test("Demo expect" , async() => {
+    expect(1 + 2).toEqual(3);
+
+    //Expect array length
+    const arr = [1, 2, 3];
+    expect(arr).toHaveLength(3);
+
+    //Expect string length
+    const str = "Hello VN";
+    expect(str).toContain("Hello");
+
+});
+
+test("Material page" , async ({ page }) => {
+    await page.goto("https://material.playwrightvn.com/");
+
+    const title = await page.title();
+    expect(title).toContain("Playwright VietNam");
+
+});
+
+test("Material page - non web-first" , async({ page }) => {
+    await page.goto("https://material.playwrightvn.com/019-enable-form.html");
+    await page.waitForTimeout(1_000);
+
+    const isVisible = await page.locator("//button[@id='submitButton']").isEnabled();
+    expect(isVisible).toEqual(true);
+
+});
+
+test("Material page - web-first" , async({ page }) => {
+    await page.goto("https://material.playwrightvn.com/019-enable-form.html");
+    
+
+    const submitButton = page.locator("//button[@id='submitButton']");
+    await expect(submitButton).toBeEnabled({timeout: 7_000 });
+
+});
+
+test("Material page - demo to have class" , async({ page }) => {
+    await page.goto("https://material.playwrightvn.com/01-xpath-register-page.html");
+    
+
+    const containerLocator = page.locator("//div[@id='ancestor']");
+    await expect(containerLocator).toHaveClass("container" , {timeout: 5_000 });
+
+});
+
+
+
+
+
+
+
+
